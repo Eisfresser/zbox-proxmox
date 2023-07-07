86800e2f1eed7fded05c2c9920c3395437abece0
Bioyina <rolf.moser@gmail.com>
2023-07-07 14:13:20 +0200

    changes.py added

--- files/backup-pull.sh
+++ files/backup-pull.sh
@@ -3,7 +3,9 @@
 # A script to perform incremental backups using rsync
 # Run on Jabba (NAS) to pull data from Dagobert (server)
 
-# Copy to jabba: scp -O backup-pull.sh rsync@jabba:/var/services/homes/rsync/backup-pull.sh
+# Copy to jabba: 
+#   scp -O backup-pull.sh rsync@jabba:/var/services/homes/rsync/backup-pull.sh
+#   scp -O changes.py     rsync@jabba:/var/services/homes/rsync/changes.py
 
 set -o errexit
 set -o nounset
@@ -28,8 +30,6 @@ echo "Latest link: ${LATEST_LINK}"
 echo "Logfile: ${LOGFILE}"
 echo "rsync version: $(rsync --version | head -n 1)"
 echo "----------------------------------------"
-cat $0 >> ${LOGFILE}
-echo "----------------------------------------" >> ${LOGFILE}
 mkdir -p "${BACKUP_DIR}"
 
 rsync -av \
@@ -38,7 +38,8 @@ rsync -av \
     --copy-links \
     --exclude="@eaDir" \
     --exclude=".DS_Store" \
-    "${BACKUP_PATH}" >> ${LOGFILE}
+    --exclude="._*" \
+    "${BACKUP_PATH}" 
 
 rm -rf "${LATEST_LINK}"
 ln -s "${BACKUP_PATH}" "${LATEST_LINK}"
@@ -46,3 +47,9 @@ ELAPSED=$(( SECONDS - START ))
 echo "----------------------------------------"
 df -h ${BACKUP_DIR}
 echo "$(date) Backup completed in $ELAPSED seconds"
+echo "----------------------------------------"
+python changes.py
+echo "----------------------------------------"
+
+2023-06-10_04:10:35  2023-06-29_04:10:02  2023-07-01_11:18:56  2023-07-03_04:10:01  2023-07-05_04:10:01  2023-07-07_04:10:01  latest
+2023-06-12_04:10:02  2023-06-30_15:19:05  2023-07-02_04:10:36  2023-07-04_04:10:36  2023-07-06_04:10:01  2023-07-07_12:26:32
