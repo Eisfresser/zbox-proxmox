73626e2e0d23ae75b52928c91768e275784533b9
Bioyina <rolf.moser@gmail.com>
2023-09-25 13:43:13 +0200

    mv plex..media to downloads becuase of size

--- autopirate/deploy.sh
+++ autopirate/deploy.sh
@@ -4,3 +4,9 @@
 scp autopirate/*.yml root@autopirate.local:/autopirate/
 scp autopirate/homepage/*.yaml root@autopirate.local:/autopirate/config/homepage/
 scp autopirate/autopirate.sh root@autopirate.local:/autopirate/
+
+ssh root@autopirate.local "mkdir -p /autopirate/downloads/plex-transcode"
+ssh root@autopirate.local "mkdir -p /autopirate/downloads/plex-media"
+ssh root@autopirate.local "mkdir -p /autopirate/downloads/jellyfin-transcode:"
+ssh root@autopirate.local "mkdir -p /autopirate/config/plex/Library/Application Support/Plex Media Server"
+ssh root@autopirate.local "ln -s /autopirate/downloads/plex-media /autopirate/config/plex/Library/Application\ Support/Plex\ Media\ Server/Media"
