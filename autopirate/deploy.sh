690d420a65375fd944c295284d97e80cfba83f4d
Bioyina <rolf.moser@gmail.com>
2023-10-19 16:29:33 +0200

    add picam

--- autopirate/deploy.sh
+++ autopirate/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # sync autopirate
-scp autopirate/*.yml root@autopirate.local:/autopirate/
-scp autopirate/homepage/*.yaml root@autopirate.local:/autopirate/config/homepage/
-scp autopirate/autopirate.sh root@autopirate.local:/autopirate/
+scp *.yml root@autopirate.local:/autopirate/
+scp homepage/*.yaml root@autopirate.local:/autopirate/config/homepage/
+scp autopirate.sh root@autopirate.local:/autopirate/
 
 ssh root@autopirate.local "mkdir -p /autopirate/downloads/plex-transcode"
 ssh root@autopirate.local "mkdir -p /autopirate/downloads/plex-media"
