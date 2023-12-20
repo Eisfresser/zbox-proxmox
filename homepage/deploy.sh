f61e401bfe665948800d3077405918bae29cd2ab
eisfresser <eisfresser@gmail.com>
2023-12-20 17:53:04 +0100

    add option to pull

--- homepage/deploy.sh
+++ homepage/deploy.sh
@@ -4,8 +4,11 @@
 ssh root@home.fqp.ch "mkdir -p /var/homepage"
 scp homepage/*.yaml root@home.fqp.ch:/var/homepage/
 scp homepage/*.yml root@home.fqp.ch:/var/homepage/
-ssh root@home.fqp.ch "\
-                    docker compose -f /var/homepage/homepage.yml down && \
-                    docker compose -f /var/homepage/homepage.yml pull && \
-                    docker compose -f /var/homepage/homepage.yml up -d && \
-                    docker image prune -f"
+
+if [ "$1" = "pull" ]; then
+    ssh root@home.fqp.ch "\
+                        docker compose -f /var/homepage/homepage.yml down && \
+                        docker compose -f /var/homepage/homepage.yml pull && \
+                        docker compose -f /var/homepage/homepage.yml up -d && \
+                        docker image prune -f"
+fi
