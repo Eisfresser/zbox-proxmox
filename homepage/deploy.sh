95ac8b5f79bf2034033e10858e42694b7aa5f8f1
eisfresser <eisfresser@gmail.com>
2024-03-29 19:15:23 +0100

    rename docker compose file

--- homepage/deploy.sh
+++ homepage/deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-
-ssh root@home.fqp.ch "mkdir -p /var/homepage"
-scp homepage/*.yaml root@home.fqp.ch:/var/homepage/
-scp homepage/*.yml root@home.fqp.ch:/var/homepage/
+pushd .
+cd ~/Projects/zbox-proxmox/homepage
+ssh root@home.fqp.ch "mkdir -p /var/homepage/icons"
+scp *.yaml root@home.fqp.ch:/var/homepage/
+scp *.yml root@home.fqp.ch:/var/homepage/
+scp ./icons/* root@home.fqp.ch:/var/homepage/icons/
 
 if [ "$1" = "pull" ]; then
     ssh root@home.fqp.ch "\
-                        docker compose -f /var/homepage/homepage.yml down && \
-                        docker compose -f /var/homepage/homepage.yml pull && \
-                        docker compose -f /var/homepage/homepage.yml up -d && \
+                        docker compose -f /var/homepage/docker-compose.yml down && \
+                        docker compose -f /var/homepage/docker-compose.yml pull && \
+                        docker compose -f /var/homepage/docker-compose.yml up -d && \
                         docker image prune -f"
 fi
+popd
